e2d96d12b3741a1fc44650ec8ac5be3827cd9c30
MahdiYosofzay <mahdiyosofzay@gmail.com>
2022-08-04 03:54:18 +0430

    some changes have made

--- client/src/UIComponents/NewOrder.js
+++ client/src/UIComponents/NewOrder.js
@@ -1,9 +1,11 @@
-import React from 'react';
-import ItemList from './ItemList';
-import { Link } from 'react-router-dom';
+import React, { useEffect } from "react";
+import ItemList from "./ItemList";
+import { Link } from "react-router-dom";
+import { connect } from "react-redux";
+import CircularProgress from "@mui/material/CircularProgress";
 
-const NewOrder = ({ match }) => {
-  let category = '';
+const NewOrder = ({ match, categories }) => {
+  let category = "";
   if (match.params.category) category = match.params.category;
 
   return (
@@ -11,14 +13,22 @@ const NewOrder = ({ match }) => {
       <div className="sidebar">
         <h3>Filter</h3>
         <Link to="/new-order">All</Link>
-        <Link to="/new-order/burger">Burgers</Link>
-        <Link to="/new-order/chicken">Chicken</Link>
-        <Link to="/new-order/sandwich">Sandwiches</Link>
-        <Link to="/new-order/drink">Drinks</Link>
+        {categories ? (
+          categories?.map((cat) => (
+            <Link key={cat._id} to={`/new-order/${cat.name}`}>
+              {cat.name.charAt(0).toUpperCase() + cat.name.slice(1)}
+            </Link>
+          ))
+        ) : (
+          <CircularProgress />
+        )}
       </div>
       <ItemList category={category} />
     </div>
   );
 };
 
-export default NewOrder;
+const mapStateToProps = (state) => ({
+  categories: state.categories,
+});
+export default connect(mapStateToProps)(NewOrder);
--- config/database.js
+++ config/database.js
@@ -1,16 +1,18 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+require("dotenv").config();
 
 const dbConnection = async () => {
   try {
     await mongoose
-      .connect('mongodb://localhost/dehkadahDB', {
+      .connect(process.env.DB_CONNECTION, {
+        dbName: "dehkadeh",
         useUnifiedTopology: true,
         useNewUrlParser: true,
         useFindAndModify: false,
         useCreateIndex: true,
       })
-      .then(() => console.log('connected to mongo server'))
-      .catch((err) => console.error('could not connect to mongo server', err));
+      .then(() => console.log("connected to mongo server"))
+      .catch((err) => console.error("could not connect to mongo server", err));
   } catch (err) {
     console.log(err.message);
     process.exit(1);
